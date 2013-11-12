daebc3b8ce96716b0bb3678061df1340c2d25ff1
Andrew Doyle <andrew.doyle.83@gmail.com>
2013-11-12 18:06:22 +1100

    basic commit

---
+++ src/popup.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    document.domain = "http://www.rfs.nsw.gov.au";
+    var url = "http://www.rfs.nsw.gov.au/feeds/majorIncidents.xml";
+    var req = new XMLHttpRequest();
+    req.open("GET", 'url', true);
+    req.onload = parseXml(req);
+
+    function parseXml(data) {
+        alert("blah");
+    }
+
+
+
+});
