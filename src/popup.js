234e0041a00716ffdda28ff61d2e4c95315b40a1
Andrew Doyle <andrew.doyle.83@gmail.com>
2013-12-01 09:43:10 +1100

    basic functionality still needs work

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,46 @@
 ﻿$(function () {
-    document.domain = "http://www.rfs.nsw.gov.au";
-    var url = "http://www.rfs.nsw.gov.au/feeds/majorIncidents.xml";
-    var req = new XMLHttpRequest();
-    req.open("GET", 'url', true);
-    req.onload = parseXml(req);
+    
+    //const url = "https://rfsforchromeservice.apphb.com/fires";
+    const url = "http://localhost/rfsforchrome.service/fires";
+    
+    $(window).load(function() {
+        loadData()
+    });
+
+    function loadData() {
+        $.ajax({
+                type: 'GET',
+                url: url,
+                contentType: 'jsonp',
+                dataType: 'jsonp',
+                success: function( data ) {
+                    parseXml(data);
+                }
+            });
+    }
+
 
     function parseXml(data) {
-        alert("blah");
+        for (var i = 0; i < data.length; i++) {
+            //$("#results").append(data[i].Title + " - ");
+            var item = data[i];
+            var markup = appendMarkup(item);
+            $("#results").append(markup);
+        };        
+  		
     }
+
+    function appendMarkup(item) {
+        var category = item.Category.toLowerCase();
+        var item =        
+            '<div class="item">' +
+                '<div class="box ' + category +'"></div>' +
+                '<div class="title">' + item.Title +'</div>' +
+                '<div class="status">' + item.Status +'</div>' +
+            '</div>';
+        return item;
+    }
+
 
 
 
